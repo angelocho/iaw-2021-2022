fba12c595bc8da4af77398836d93decee07ba97e
Ángel Bocalandro <angelocho64@gmail.com>
2022-01-17 11:58:41 +0100

    Introduccion

---
+++ 01-Introduccion/index.php
@@ -0,0 +1,74 @@
+<?php 
+$miprimeravariable=5;
+?>
+
+
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/index.css">
+    <title>Pagina de introduccion</title>
+</head>
+<body>
+    <div id="cabecera">
+    <h1>Esto es la cabecera</h1> 
+    <?php 
+    echo $miprimeravariable;
+    ?>    
+</div>
+<div id="contenido">
+    <div class="noticia">
+        <h1>Titulo</h1>
+        <img src="https://picsum.photos/100/150" alt="">
+        Lorem fistrum está la cosa muy malar quis qué dise usteer no puedor tempor. Aliqua esse va usté muy cargadoo tiene musho peligro officia. Sit amet está la cosa muy malar ese que llega aute hasta luego Lucas commodo occaecat tempor a peich sexuarl. Diodeno commodo jarl velit cillum quietooor. Elit caballo blanco caballo negroorl diodenoo duis aute consectetur te voy a borrar el cerito aute. Apetecan ut me cago en tus muelas diodenoo consectetur nisi hasta luego Lucas cillum exercitation ahorarr. Ese hombree no puedor jarl fistro adipisicing eiusmod.
+    </div>
+</div>
+<div id="contenido">
+    <div class="noticia">
+        <h1>Titulo</h1>
+        <img src="https://picsum.photos/100/150" alt="">
+        Lorem fistrum está la cosa muy malar quis qué dise usteer no puedor tempor. Aliqua esse va usté muy cargadoo tiene musho peligro officia. Sit amet está la cosa muy malar ese que llega aute hasta luego Lucas commodo occaecat tempor a peich sexuarl. Diodeno commodo jarl velit cillum quietooor. Elit caballo blanco caballo negroorl diodenoo duis aute consectetur te voy a borrar el cerito aute. Apetecan ut me cago en tus muelas diodenoo consectetur nisi hasta luego Lucas cillum exercitation ahorarr. Ese hombree no puedor jarl fistro adipisicing eiusmod.
+    </div>
+</div><div id="contenido">
+    <div class="noticia">
+        <h1>Titulo</h1>
+        <img src="https://picsum.photos/100/150" alt="">
+        Lorem fistrum está la cosa muy malar quis qué dise usteer no puedor tempor. Aliqua esse va usté muy cargadoo tiene musho peligro officia. Sit amet está la cosa muy malar ese que llega aute hasta luego Lucas commodo occaecat tempor a peich sexuarl. Diodeno commodo jarl velit cillum quietooor. Elit caballo blanco caballo negroorl diodenoo duis aute consectetur te voy a borrar el cerito aute. Apetecan ut me cago en tus muelas diodenoo consectetur nisi hasta luego Lucas cillum exercitation ahorarr. Ese hombree no puedor jarl fistro adipisicing eiusmod.
+    </div>
+</div><div id="contenido">
+    <div class="noticia">
+        <h1>Titulo</h1>
+        <img src="https://picsum.photos/100/150" alt="">
+        Lorem fistrum está la cosa muy malar quis qué dise usteer no puedor tempor. Aliqua esse va usté muy cargadoo tiene musho peligro officia. Sit amet está la cosa muy malar ese que llega aute hasta luego Lucas commodo occaecat tempor a peich sexuarl. Diodeno commodo jarl velit cillum quietooor. Elit caballo blanco caballo negroorl diodenoo duis aute consectetur te voy a borrar el cerito aute. Apetecan ut me cago en tus muelas diodenoo consectetur nisi hasta luego Lucas cillum exercitation ahorarr. Ese hombree no puedor jarl fistro adipisicing eiusmod.
+    </div>
+</div>
+
+<footer>
+    cosas del pie de pagina
+
+</footer>
+
+
+  <!--<span>
+        <div>
+            <div>
+                <h1>No soy hijo directo de span</h1>
+
+            </div>
+        </div>
+    </span>
+
+</div>
+<div>
+    <span>
+                <h1>Soy hijo directo de span</h1>
+    </span>
+
+</div>
+-->
+
+</body>
+</html>
