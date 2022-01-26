91ce064c11f83318409152b4f7a9dc49643b3e81
Ángel Bocalandro <angelocho64@gmail.com>
2022-01-26 13:26:02 +0100

    Piramide bien hecha

--- 02-formulario1/index.php
+++ 02-formulario1/index.php
@@ -1,57 +1,14 @@
-<?php
-
-#Utilizo una funcion que dice si hay una variable establecida y
-#sacamos el valor del objeto
- #if(isset($_POST["enviar"])){
- #   echo "Se esta enviando info " - $_POST["nombre"];
-#}else{
- #   echo "Bienvenido";
-
-#}
-
+<?php 
 $bienvenido=true;
-$nombre="";
-$apellidos='';
-$edad='';
-$edad_20_39='';
-$edad_40_59='';
-$edad_60_79='';
+$asterisco="*";
+$numero="";
 
-if(isset($_POST["Enviar"])){
+#print_r($_POST);
+
+if(isset($_POST["enviar"])){
     $bienvenido=false;
-    $nombre=$_POST["nombre"];
-    $apellidos=$_POST["apellidos"];
-    #$edad=$_POST["edad"];
-    if(isset($_POST["edad"])){
-        /*if($_POST["edad"]=="20-39"){
-                $edad_20_39='checked';
-        }elseif ($_POST["edad"]=="40-59"){
-                $edad_40_59='checked';
-        }elseif ($_POST["edad"]=="60-79"){
-                $edad_60_79='checked';
-        } */
-        # If ternarios
-        # $variable=(true)?'valorverdad':'valormentira';
-     /* $edad_20_39=($_POST["edad"]=="20_39")?'checked':'';
-      $edad_40_59=($_POST["edad"]=="40_59")?'checked':'';
-      $edad_60_79=($_POST["edad"]=="60_79")?'checked':'';
-    */
-
-    ${"edad_".$_POST["edad"]}='checked';
-    
-    }
+    $numero=$_POST["numero"];   
 }
-
-#echo "GET:<br>";
-#print_r($_GET);
-echo "POST:<br>";
-print_r($_POST);
-#echo "COOKIES:<br>";
-#print_r($_COOKIES);
-#echo "SESSION:<br>";
-#print_r($_SESSION);
-#echo "SERVER:<br>";
-#print_r($_SERVER);
 
 ?>
 
@@ -61,36 +18,32 @@ print_r($_POST);
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Formulario</title>
+    <title>Piramide</title>
 </head>
 <body>
-<?php if($bienvenido) { ?>
+        <!-- Hay que hacer un txt number y un boton, 
+        una vez se envie solo tiene que mostrar 
+        las piramides
+        -->
+        <?php if($bienvenido) { ?>
         <h1>Bienvenido!!!</h1>
-    <?php } ?>
-    <div>
-        <form action="index.php" method="POST">    
-<fieldset>
-                <legend>Información Personal</legend>
-                <label for="nombre">Nombre:</label>
-                <input name="nombre" id="nombre" type="text" tabindex="1" value="<?=$nombre?>"/>
-                <label for="apellidos" >Apellidos:</label>
-                <input name="apellidos" id="apellidos" type="text" tabindex="2" value="<?=$apellidos?>"/>
-            </fieldset>
+        <form action="index.php" method=POST>
+        <input type="number" name="numero" id="numero">
+        <input type="submit" name="enviar" value="enviar">
+        <?php } ?>
+        
+        <?php if($bienvenido == false){ 
+            for ($i=1; $i <= $numero; $i++){
+                for($c=1; $c <= $i; $c++){
+                echo "<span style=color:"."#".rand(0,999999).">".$asterisco."</span>"; 
+                }
+                echo "<br>";
+            }
+         ?>
 
-            <fieldset>
-                <legend>Edad</legend>
-            <!--    <label><input type="radio" tabindex="20" name="edad" value="20-39" <?php if($edad == "20-39"){echo "checked"; } ?>/> 20-39</label>
-                <label><input type="radio" tabindex="21" name="edad" value="40-59" <?php if($edad == "40-59"){echo "checked"; } ?>/> 40-59</label>
-                <label><input type="radio" tabindex="22" name="edad" value="60-79" <?php if($edad == "60-79"){echo "checked"; } ?>/> 60-79</label>-->
-                <label><input type="radio" tabindex="20" name="edad" value="20_39" <?=$edad_20_39?>/> 20-39</label>
-                <label><input type="radio" tabindex="21" name="edad" value="40_59" <?=$edad_40_59?>/> 40-59</label>
-                <label><input type="radio" tabindex="22" name="edad" value="60_79" <?=$edad_60_79?>/> 60-79</label>
-            
-            </fieldset>
 
-            <input type="submit" name="Enviar" value="Enviar">
+        <?php } ?>
+
         </form>
-    </div>
-
 </body>
 </html>
---
+++ 02-formulario1/index1.php
@@ -0,0 +1,96 @@
+<?php
+
+#Utilizo una funcion que dice si hay una variable establecida y
+#sacamos el valor del objeto
+ #if(isset($_POST["enviar"])){
+ #   echo "Se esta enviando info " - $_POST["nombre"];
+#}else{
+ #   echo "Bienvenido";
+
+#}
+
+$bienvenido=true;
+$nombre="";
+$apellidos='';
+$edad='';
+$edad_20_39='';
+$edad_40_59='';
+$edad_60_79='';
+
+if(isset($_POST["Enviar"])){
+    $bienvenido=false;
+    $nombre=$_POST["nombre"];
+    $apellidos=$_POST["apellidos"];
+    #$edad=$_POST["edad"];
+    if(isset($_POST["edad"])){
+        /*if($_POST["edad"]=="20-39"){
+                $edad_20_39='checked';
+        }elseif ($_POST["edad"]=="40-59"){
+                $edad_40_59='checked';
+        }elseif ($_POST["edad"]=="60-79"){
+                $edad_60_79='checked';
+        } */
+        # If ternarios
+        # $variable=(true)?'valorverdad':'valormentira';
+     /* $edad_20_39=($_POST["edad"]=="20_39")?'checked':'';
+      $edad_40_59=($_POST["edad"]=="40_59")?'checked':'';
+      $edad_60_79=($_POST["edad"]=="60_79")?'checked':'';
+    */
+
+    ${"edad_".$_POST["edad"]}='checked';
+    
+    }
+}
+
+#echo "GET:<br>";
+#print_r($_GET);
+echo "POST:<br>";
+print_r($_POST);
+#echo "COOKIES:<br>";
+#print_r($_COOKIES);
+#echo "SESSION:<br>";
+#print_r($_SESSION);
+#echo "SERVER:<br>";
+#print_r($_SERVER);
+
+?>
+
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Formulario</title>
+</head>
+<body>
+<?php if($bienvenido) { ?>
+        <h1>Bienvenido!!!</h1>
+    <?php } ?>
+    <div>
+        <form action="index.php" method="POST">    
+<fieldset>
+                <legend>Información Personal</legend>
+                <label for="nombre">Nombre:</label>
+                <input name="nombre" id="nombre" type="text" tabindex="1" value="<?=$nombre?>"/>
+                <label for="apellidos" >Apellidos:</label>
+                <input name="apellidos" id="apellidos" type="text" tabindex="2" value="<?=$apellidos?>"/>
+            </fieldset>
+
+            <fieldset>
+                <legend>Edad</legend>
+            <!--    <label><input type="radio" tabindex="20" name="edad" value="20-39" <?php if($edad == "20-39"){echo "checked"; } ?>/> 20-39</label>
+                <label><input type="radio" tabindex="21" name="edad" value="40-59" <?php if($edad == "40-59"){echo "checked"; } ?>/> 40-59</label>
+                <label><input type="radio" tabindex="22" name="edad" value="60-79" <?php if($edad == "60-79"){echo "checked"; } ?>/> 60-79</label>-->
+                <label><input type="radio" tabindex="20" name="edad" value="20_39" <?=$edad_20_39?>/> 20-39</label>
+                <label><input type="radio" tabindex="21" name="edad" value="40_59" <?=$edad_40_59?>/> 40-59</label>
+                <label><input type="radio" tabindex="22" name="edad" value="60_79" <?=$edad_60_79?>/> 60-79</label>
+            
+            </fieldset>
+
+            <input type="submit" name="Enviar" value="Enviar">
+        </form>
+    </div>
+
+</body>
+</html>
