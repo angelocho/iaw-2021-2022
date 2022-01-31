a978f4ceebfefef52facee6f6fd6bc68298e65ed
Ángel Bocalandro <angelocho64@gmail.com>
2022-01-31 10:18:55 +0100

    Muchos cambios

--- 03-piramide/index.php
+++ 03-piramide/index.php
@@ -1,14 +1,15 @@
 <?php 
-$bienvenido=true;
+$enviado=false;
 $asterisco="*";
 $numero="";
 
 #print_r($_POST);
 
-if(isset($_POST["enviar"])){
-    $bienvenido=false;
+if(isset($_POST["numero"])){
     $numero=$_POST["numero"];   
+    $enviado=true;
 }
+
 
 ?>
 
@@ -16,8 +17,15 @@ if(isset($_POST["enviar"])){
 <html lang="es">
 <head>
     <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+ <?php   if(isset($_POST["numero"])){ 
+
+
+echo '<meta http-equiv="refresh" content="1;url=http://localhost:9000/index.php?&numero='. $numero . '">';          
+     ?>
+<?php } ?>
+    
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <link rel="stylesheet" href="indexstyle.css">
     <title>Piramide</title>
 </head>
 <body>
@@ -25,25 +33,26 @@ if(isset($_POST["enviar"])){
         una vez se envie solo tiene que mostrar 
         las piramides
         -->
-        <?php if($bienvenido) { ?>
+        <div id=contenido>
+        <?php if($enviado == false) { ?>
         <h1>Bienvenido!!!</h1>
         <form action="index.php" method=POST>
         <input type="number" name="numero" id="numero">
         <input type="submit" name="enviar" value="enviar">
-        <?php } ?>
         
-        <?php if($bienvenido == false){ 
+        <?php } else { 
             for ($i=1; $i <= $numero; $i++){
                 for($c=1; $c <= $i; $c++){
-                echo "<span style=color:"."#".rand(0,999999).">".$asterisco."</span>"; 
-                }
+                #echo "<span style=color:"."#".rand(0,999999).">".$asterisco."</span>"; 
+                echo "<span style=color:".sprintf("#%06X", mt_rand(0, 0xFFFFFF)).">".$asterisco."</span>"; 
+  
+            }
                 echo "<br>";
             }
          ?>
-
-
         <?php } ?>
 
         </form>
+        </div>
 </body>
 </html>
